a973b5ed5044f3ffebec631b959b64f7b5c3ca9b
A_A <a-a@aa-Aspire-E5-771>
2018-07-29 12:15:36 +0200

    Fix heroku error

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('See you later alligator'));
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Started app on port ${port}`));
