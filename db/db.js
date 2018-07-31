b05177439d73a5767a4d4ad133751d58fbe8dff8
A_A <21040751+Otto-AA@users.noreply.github.com>
2018-07-31 09:52:05 +0200

    Add connect, disconnect and logDatabase to db.js

--- db/db.js
+++ db/db.js
@@ -3,67 +3,37 @@ const User = require('./models/User');
 const SignupVerification = require('./models/SignupVerification');
 
 const mongo_url = process.env.MONGO_URL;
-mongoose.connect(mongo_url, (err) => {
-    if (err) {
-        console.error('Error while connecting to database');
-        throw err;
-    }
-});
 
-
-
-
-User.find()
-    .then(res => {
-        console.group('Users');
-        console.log(res);
-        console.groupEnd();
+function connect () {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(mongo_url, (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
     });
-SignupVerification.find()
-    .then(res => {
-        console.group('SignupVerifications');
-        console.log(res);
-        console.groupEnd();
-    });
+}
+
+function disconnect = mongoose.disconnect;
+
+function logDatabase() {
+    User.find()
+        .then(res => {
+            console.group('Users');
+            console.log(res);
+            console.groupEnd();
+        });
+    SignupVerification.find()
+        .then(res => {
+            console.group('SignupVerifications');
+            console.log(res);
+            console.groupEnd();
+        });
+}
 
 module.exports = {
-    User,
-    SignupVerification
+    connect,
+    disconnect,
+    logDatabase
 };
-
-/**
- * Database structure
- * 
- * users: {
- *   user_id: {
- *     user_name: string
- *     created_on: date
- *     email: string
- *     password: hashed string
- *   }
- * }
- * 
- * songs: {
- *   song_id: {
- *     user_id: string
- *     song_name: string
- *     song_number: integer
- *     created_on: string
- *     composers: array<string>
- *     files: {
- *       mp3: absolute url || None
- *       midi: absolute url || None
- *       pdf: absolute url || None
- *     }
- *     design: design_id
- *   }
- * }
- * 
- * designs: {
- *   design_id: {
- *     ...
- *   }
- * }
- * 
- */
-
