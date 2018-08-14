f0d24513b9e18a940c9d675e3cbacc40f02def46
A_A <21040751+Otto-AA@users.noreply.github.com>
2018-08-14 15:10:54 +0200

    Create parent visualization model

---
+++ src/models/Visualization.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import * as autopopulate from "mongoose-autopopulate"
+
+// TODO: This is untested. Remove me when this works
+
+export type VisualizationModel = mongoose.Document & {
+  visualizationType: string,
+  usedBySongs: string[],
+  data: { [k: string]: any }
+};
+
+const visualizationSchema = new mongoose.Schema({
+  visualizationType: {
+    type: String,
+    enum: [ "standard" ]
+  },
+  usedBySongs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Song'
+  }],
+  data: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'visualizationType',
+    autopopulate: true,
+    required: true
+  }
+}, { timestamps: true, strict: true });
+visualizationSchema.plugin(autopopulate);
+
+const Visualization = mongoose.model("Visualization", visualizationSchema);
+export default Visualization;
