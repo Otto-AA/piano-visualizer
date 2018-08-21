44f8917008ecd09db6773c5e69c49c2497fe60b8
A_A <21040751+Otto-AA@users.noreply.github.com>
2018-08-21 14:43:40 +0200

    Rewrite song.factory to use SchemaDataFactory

--- test/factory/song.factory.ts
+++ test/factory/song.factory.ts
@@ -1,6 +1,52 @@
-import Song from "../../src/models/Song";
+import { SongData } from "../../src/models/Song";
+import { SchemaDataFactory } from "./SchemaDataFactory";
 
+const songSchema = {
+    id: "song",
+    type: "object",
+    properties: {
+        name: {
+            type: "string",
+            faker: "random.word"
+        },
+        type: {
+            type: "string",
+            enum: ["composition", "improvisation", "cover"]
+        },
+        mp3Link: {
+            type: "string",
+            faker: "internet.url"
+        },
+        midLink: {
+            type: "string",
+            faker: "internet.url"
+        },
+        pdfLink: {
+            type: "string",
+            faker: "internet.url"
+        },
+        externalSongLink: {
+            type: "string",
+            faker: "internet.url"
+        },
+        visualizations: {
+            type: "array",
+            items: [{
+                type: "object",
+                properties: {
+                    visualizationType: "standard",
+                    visualization: "visualizationStandardId"
+                },
+                required: ["visualizationType", "visualization"]
+            }]
+        }
+    },
+    required: ["name", "type", "mp3Link", "midLink", "visualizations"]
+};
+            
+export const songFactory = new SchemaDataFactory<SongData>(songSchema);
 
+/*
 export const songData = {
     name: "mysongname22",
     type: "composition",
@@ -14,3 +60,4 @@ export const songData = {
         visualization: "5b741d288b95041d9c171aaa"
     }]
 };
+*/
